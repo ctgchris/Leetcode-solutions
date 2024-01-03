14ef46e85c9d0f7d86d84c23d9705c95a88f8cd5
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2024-01-02 23:19:48 -0500

    Time: 162 ms (9.02%), Space: 22 MB (29.24%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,9 +1,8 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        dict = {}
+        c = {}
         
-        # Count occurrences manually
         for num in nums:
-            count_dict[num] = count_dict.get(num, 0) + 1
+            c[num] = c.get(num, 0) + 1
         c=sorted(c, key=lambda x: c[x], reverse=True)
         return c[:k]
