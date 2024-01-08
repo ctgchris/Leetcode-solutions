cf2f8ae8641174494ec05c4b3c030560d0663386
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2024-01-08 09:43:05 -0500

    Time: 39 ms (94.66%), Space: 17.7 MB (54.50%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new = ''
+        for c in s:
+            if c.isalpha() or c.isdigit():
+                new += c.lower()
+        return (new == new[::-1])
