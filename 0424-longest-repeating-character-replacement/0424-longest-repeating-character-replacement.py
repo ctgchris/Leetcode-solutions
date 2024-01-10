069092bd0eca7be779d4e7d50bbdbdfb7474016b
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2024-01-10 18:35:36 -0500

    Time: 94 ms (72.49%), Space: 17.6 MB (13.79%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,11 +1,30 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        maxf = res = 0
-        count = collections.Counter()
-        for i in range(len(s)):
-            count[s[i]] += 1
-            maxf = max(maxf, count[s[i]])
-            if res - maxf < k: res += 1
-            else: count[s[i - res]] -= 1
-        return res
+        count = {}
+        maxSlidingWindow = 0 
+        
+        # left pointer 
+        l = 0 
+        for r in range(len(s)): 
+            # right pointer is moving forward
+            
+           
+            
+            # add 1 to the count of the character we are trying to add to the sliding window
+            # i.e [ABC] A --> [ABCA]
+            count[s[r]] = 1 + count.get(s[r],0)
+            
+            # is the sliding window invalid if we add the new character?
+            if ( (r-l+ 1) - max( count.values()) ) > k:
+                
+                # if it is reduce the frequency of the character from the count 
+                count[s[l]] -= 1
+                
+                # and we move the window from the left and
+                l += 1
+                 
+            # update the max of the sliding window
+            maxSlidingWindow = max(maxSlidingWindow,(r - l + 1))
+            
+        return maxSlidingWindow
             
