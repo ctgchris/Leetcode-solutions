0f94f3eefe092defc0821a929b394003a838407b
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2024-02-22 22:19:20 -0500

    Time: 65 ms (52.39%), Space: 17.7 MB (67.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,11 +2,11 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:   
         #brute force check each pair (n^3)
 
-        num_dict = {}  # Using a meaningful variable name
-        for i, num in enumerate(nums):  # Using enumerate to get both value and index
-            diff = target - num
-            if diff in num_dict:
-                return [num_dict[diff], i]  # Returning indices
+        hash_map={}
+        for ind, num in enumerate(nums):
+            diff=target-num
+            if diff in hash_map:
+                return [hash_map[diff], ind]
             else:
-                num_dict[num] = i  # Storing value-index pair in the dictionary
-        return []  # If no solution is found, you can return an empty list or None
+                hash_map[num]=ind
+        return []
