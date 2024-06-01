3d7344ce1b736894779d45af28f211ac9bcc06f2
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2024-06-01 02:07:02 -0400

    Time: 1656 ms (31.68%), Space: 17.2 MB (97.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:   
-        #brute force check each pair (n^3)
-
-        hash_map={}
-        for ind, num in enumerate(nums):
-            diff=target-num
-            if diff in hash_map:
-                return [hash_map[diff], ind]
-            else:
-                hash_map[num]=ind
-        return []
+        
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i]+ nums[j]==target:
+                    return [i,j]
+                
