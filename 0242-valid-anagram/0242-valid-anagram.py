5ee01d1da0476a777b092e411e07c10e397eecca
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2024-06-01 23:08:00 -0400

    Time: 39 ms (33.65%), Space: 13.4 MB (25.81%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,26 +8,13 @@ class Solution(object):
         if len(s)!= len(t):
             return False
         sHash = {}
-        
-        for c in s:
-            if c in sHash:
-                sHash[c] +=1
-            else:
-                sHash[c]=1
-                
-        for c in t:
-            if c in sHash:
-                if sHash >0:
-                    sHash[c]-=1
-                else:
-                    return False
-            else:
-                return False
+        tHash= {}
+        for i in range(len(s)):
+            sHash[s[i]]= 1 + sHash.get(s[i], 1)
+            tHash[t[i]]= 1 + tHash.get(t[i], 1)
             
-        if all(value == 0 for value in sHash.values()):
-            return True
-        else:
-            return False
+        return sHash==tHash
+
             
             
         
