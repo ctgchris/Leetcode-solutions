c02261b92136eb7497141e4eb409f5edb115340e
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2024-11-28 11:13:23 -0500

    Time: 5 ms (54.57%), Space: 17.3 MB (16.45%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,21 +1,45 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        
+        #iterate through and if encounter a I,X,C check next index if its a respective instance
+        #then subtract 1st number from 2nd number. move index 2 spaces if case is hit.
+
         res=0
-        end=len(s)
+        mapS={'I': 1, 'V': 5, 'X': 10, 'L':50, 'C':100, 'D': 500,'M':1000}
         i=0
-        d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        while i < end:
-            if i+1 < end and d[s[i+1]] > d[s[i]]:
-                res+= d[s[i+1]] - d[s[i]]
-                i+=2
+        while i<len(s):
+            if i<len(s)-1:
+
+                if s[i] not in ['I', 'X', 'C']:
+                    res+=mapS[s[i]]
+                    i+=1
+                else:
+                    
+                    if s[i] == 'I':    
+                        if s[i+1] in ['V', 'X']:
+                            res+=mapS[s[i+1]]-mapS[s[i]]
+                            i+=2
+                        else:
+                            res+=mapS[s[i]]
+                            i+=1
+
+                    elif s[i] == 'X':
+                        if s[i+1] in ['L', 'C']:
+                            res+=mapS[s[i+1]]-mapS[s[i]]
+                            i+=2
+                        else:
+                            res+=mapS[s[i]]
+                            i+=1
+
+                    
+                    else:
+                        if s[i+1] in ['D', 'M']:
+                            res+=mapS[s[i+1]]-mapS[s[i]]
+                            i+=2
+                        else:
+                            res+=mapS[s[i]]
+                            i+=1
             else:
-                res+=d[s[i]]
+                res+=mapS[s[i]]
                 i+=1
-        return res  
-            
-
-
-
-
-
-
+        return res
