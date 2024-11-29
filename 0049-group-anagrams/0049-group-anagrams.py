631a63b2f274354add7eae85be36d5c1beb77b9f
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2024-11-29 18:20:31 -0500

    Time: 23 ms (19.92%), Space: 22.2 MB (9.3%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        #get the ASCII values of each word and check if exists in hashmap, if yes append: key: ASCII, value: strings
-        #after iteration, then get all hash.values and add to res
+        
 
-        mapS={}
+        mapS=defaultdict(list)
 
         for s in strs:
-            sorted_s=''.join(sorted(s))
-            if sorted_s in mapS:
-                mapS[sorted_s].append(s)
-            else:
-                mapS[sorted_s]=[s]
+            count=[0] * 26
+            for char in s:
+                count[ord(char)- ord('a')]+=1
+
+            mapS[tuple(count)].append(s)
 
         return list(mapS.values())
-
