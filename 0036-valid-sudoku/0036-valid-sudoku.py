551565ab7532714f8e830af10cab4464eb1e2f89
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2024-12-14 01:31:39 -0500

    Time: 4 ms (83.98%), Space: 12 MB (23.09%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,15 +4,50 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        
-        rows=collections.defaultdict(set)
-        cols=collections.defaultdict(set)
-        squares=collections.defaultdict(set) #key = (r//3, c//3)
+        #the logic is to use a set, if != . , check if digit is in set, if so return false, if not continue. 
+        # do it for each row
+        length=len(board[0])
+        #check row
+        for r in range(9):
+            check=set()
 
-
-        for r in range(9)
             for c in range(9):
+
                 if board[r][c] != '.':
-                    if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]:
-                        return False:
-                    rows[r].add(board[r][c])
+
+                    if board[r][c] in check:
+                        return False
+                    check.add(board[r][c])
+
+
+
+        #check col
+
+        for c in range(9):
+            check=set()
+
+            for r in range(9):
+
+                if board[r][c] != '.':
+
+                    if board[r][c] in check:
+                        return False
+                    check.add(board[r][c])
+
+        starts = [(0,0), (0,3), (0,6),
+                (3,0), (3,3), (3, 6),
+                (6,0), (6,3), (6,6)]
+
+        #check box, done so by adding 
+        for row_s,col_s in starts:
+            check=set()
+            for r in range(row_s,row_s+3):
+
+                for c in range(col_s,col_s+3):
+
+                    if board[r][c] != '.':
+
+                        if board[r][c] in check:
+                            return False
+                    check.add(board[r][c])
+        return True
