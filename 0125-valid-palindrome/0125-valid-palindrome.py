83eaf234cd59b7a14efff581c1603b527415b060
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2024-12-16 03:31:41 -0500

    Time: 41 ms (5.1%), Space: 16.9 MB (83.68%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,18 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
-        l=0
-        r=len(s)-1
-
-        while l < r:
-
-            while l<r and not s[l].isalnum():
-                l+=1
-            while l<r and not s[r].isalnum():
-                r-=1
-            
-            if s[l].lower() != s[r].lower():
-                return False
-            l+=1
-            r-=1
-        return True
+        #steps
+        #l and r pointer (l < r)
+        #check if l =r then continue
+        new=""
+        for a in s:
+            if a.isalpha() or a.isdigit():
+                new+= a.lower()
+        return new == new[::-1]
