633f3c6401d4e270102f7aa3dd7e262557b0270e
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2024-12-27 06:23:46 -0500

    Time: 3 ms (95.75%), Space: 17.9 MB (11.01%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -2,11 +2,16 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         
         d={}
+        seen=set()
         # {b-b, a-a, c-a}
-        for i, n in enumerate(s):
-            if n in d and d[n] != t[i]:
-                return False
-            if n not in d and t[i] in d.values():
-                return False
-            d[n]=t[i]
+        for i, char in enumerate(s):
+            if char not in d:
+                if t[i] in seen:
+                    return False
+                d[char]=t[i]
+                seen.add(t[i])
+
+            else:
+                if d[char] != t[i]:
+                    return False
         return True
