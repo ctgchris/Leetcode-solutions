5b9a1f49fb122e032cf40dd5b772e0ec733ee656
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2025-01-30 05:09:23 -0500

    Time: 11 ms (55.39%), Space: 13 MB (63.12%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        #need to check n-1 and n+1 if its n, instantiate a left and right. however make left true if n=0 and right true
+        # if n=len(n-1). if both left and right and n == 0. add a 1  to flowerbed positon and n-=1
+        if n==0:
+            return True 
+        for i in range(len(flowerbed)):
+            left=(i==0) or (flowerbed[i-1] == 0)
+            right=(i==len(flowerbed)-1) or (flowerbed[i+1]==0)
+
+            if left and right and flowerbed[i]==0:
+                flowerbed[i]=1
+                n-=1
+                if n==0:
+                    return True
+        return n == 0
