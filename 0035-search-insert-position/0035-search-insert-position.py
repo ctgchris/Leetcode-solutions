55c1a513e2b75db17e27f21bfc696ecf68e2a689
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2025-01-30 14:08:14 -0500

    Time: 0 ms (100%), Space: 13 MB (16.97%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,20 +1,19 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        
-        #binary search
-        #[1,3,5,6] target =2
-        # l m   r
-
-        l,r =0, len(nums)
-        while l< r:
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+       
+        l=0
+        r=len(nums)
+        while l < r:
             m=(l+r)//2
-            if nums[m]<target:
-                l=m+1
-            elif nums[m] > target:
-                r=m
-            else:
+            if target==nums[m]:
                 return m
+            if target > nums[m]:
+                l=m+1
+            else:
+                r=m
         return l
-        
-
-    
