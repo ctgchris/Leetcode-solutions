1f6c252e901a18b9299a1aa7be6c1e62bf4f799d
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2025-01-30 15:16:02 -0500

    Time: 0 ms (100%), Space: 12 MB (97.35%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,50 +4,18 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        #the logic is to use a set, if != . , check if digit is in set, if so return false, if not continue. 
-        # do it for each row
-        length=len(board[0])
-        #check row
+        
+        rows=collections.defaultdict(set)
+        cols=collections.defaultdict(set)
+        squares=collections.defaultdict(set) #key = (r//3, c//3)
+
+
         for r in range(9):
-            check=set()
-
             for c in range(9):
-
                 if board[r][c] != '.':
-
-                    if board[r][c] in check:
+                    if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]:
                         return False
-                    check.add(board[r][c])
-
-
-
-        #check col
-
-        for c in range(9):
-            check=set()
-
-            for r in range(9):
-
-                if board[r][c] != '.':
-
-                    if board[r][c] in check:
-                        return False
-                    check.add(board[r][c])
-
-        starts = [(0,0), (0,3), (0,6),
-                (3,0), (3,3), (3, 6),
-                (6,0), (6,3), (6,6)]
-
-        #check box, done so by adding 
-        for row_s,col_s in starts:
-            check=set()
-            for r in range(row_s,row_s+3):
-
-                for c in range(col_s,col_s+3):
-
-                    if board[r][c] != '.':
-
-                        if board[r][c] in check:
-                            return False
-                    check.add(board[r][c])
+                    rows[r].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    squares[(r//3, c//3)].add(board[r][c])
         return True
