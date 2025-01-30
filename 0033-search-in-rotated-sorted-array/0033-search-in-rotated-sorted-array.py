361b78ab366b9a329e9acd695581e6cf80af50a0
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2025-01-30 17:05:04 -0500

    Time: 0 ms (100%), Space: 12.5 MB (78.56%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,23 +1,29 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l, r= 0, len(nums)-1
-        while l <= r:
-            middle= (l+r)//2
-            #4 cases, 
-            if nums[middle] < nums[0] and target >= nums[0]:
-                #case where out of order and target in left sorted
-                r=middle-1
-            elif nums[middle] >= nums[0] and target < nums[0]:
-                #case where out of order and target in right sorted
-                l=middle+1
-            elif target < nums[middle]:
-                r=middle-1
-
-            elif target > nums[middle]:
-                l=middle+1
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        l=0
+        r=len(nums)-1
+        while l< r:
+            m=(l+r)//2
+            if nums[m]==target:
+                return m
+            if nums[l] <= nums[m]:
+                if nums[l] <= target <= nums[m]:
+                    r=m-1                                                           
+                else:
+                    l=m+1
+            #nums[l] > nums[m], that means right of m is sorted
             else:
-                return middle
-            
-        return -1
-                
-                
+                if nums[m] <= target <=nums[r]:
+                    l=m+1
+                else:
+                    r=m-1
+        if nums[r]==target:
+            return r
+        else:
+            return -1
