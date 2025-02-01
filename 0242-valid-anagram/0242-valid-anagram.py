5fba65fd6e348fe28daa9662de97f879d8a7600a
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2025-02-01 05:02:03 -0500

    Time: 41 ms (7.91%), Space: 12.4 MB (88.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,10 +8,8 @@ class Solution(object):
         s_counts=Counter(s)
 
         for c in t:
-            if c not in s_counts:
+            if s_counts[c] ==0 or c not in s_counts:
                 return False
             s_counts[c]-=1
-            if s_counts[c]==0:
-                del s_counts[c]
 
-        return len(s_counts)==0
+        return all(count==0 for count in s_counts.values())
