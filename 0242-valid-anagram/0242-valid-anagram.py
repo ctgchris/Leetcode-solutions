7b083ec9ccf0d4fc6324169c577b3aed2c2e3faa
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2025-02-01 02:19:22 -0500

    Time: 28 ms (14.7%), Space: 13.4 MB (41.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,8 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        #could compare the sorted of the two
-        #iterate through and add to hashmap, if hashmap equal then return true
-
-        if len(s) !=len(t):
-            return False
-        mapS=dict()
-        mapT=dict()
-        for i in range(len(s)):
-            mapS[s[i]]=mapS.get(s[i], 0) + 1
-            mapT[t[i]]=mapT.get(t[i], 0 )+ 1
-        return mapS==mapT
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
