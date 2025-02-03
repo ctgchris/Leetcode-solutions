a6a05df9fc7c0ef933122b2eacd450a914a15fe6
Chris Giang <103332579+ctgchris@users.noreply.github.com>
2025-02-03 01:30:28 -0500

    Time: 14 ms (69.24%), Space: 12.6 MB (86.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,14 +5,20 @@ class Solution(object):
         :rtype: bool
         """
 
-        #2 pointers, shrink inward, nested while loop l<r and 2 while loops ensuring char is alnum, if not keep shrinking
-        #if it at any time l and r dont equal when they are both alnum, return False
-
+        # abc  cba
         l, r= 0, len(s)-1
-        while l < r:
-            while l < r and not s[l].isalnum():
+        # 
+        # while l<r
+        #while l<r and s[l] not alnum, shirnk l
+        #same for r
+        # if s[l].lower != s[r].lower return false
+        #shrink l and r
+        # add return true
+
+        while l<r:
+            while l<r and not s[l].isalnum():
                 l+=1
-            while l < r and not s[r].isalnum():
+            while l<r and not s[r].isalnum():
                 r-=1
             if s[l].lower() != s[r].lower():
                 return False
